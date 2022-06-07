7f83f1c56b07a075153e9ba8c6b4376ff7cf50bd
Wesley Henrique Araújo <56566660+wesleyharaujo@users.noreply.github.com>
2022-06-07 20:51:27 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -30,10 +30,3 @@ async function start() {
 }
 
 start();
-
-// var red, blue, reset;
-// red = '\u001b[31m';
-// blue = '\u001b[34m';
-// reset = '\u001b[0m';
-
-// console.log(red +"Aqui esta o texto em vermelho. "+ blue +"Aqui esta o texto em vermelho. "+ reset +"Aqui estamos dando reset nas cores do bash.")
